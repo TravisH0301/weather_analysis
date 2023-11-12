7b33c23a05f1e8aec449058b8e7e2beb17e5d448
Travis Hong <travis.j.hong@gmail.com>
2023-11-12 18:20:56 +1100

    add year filtering

--- dbt/macros/generate_year_partition_model.sql
+++ dbt/macros/generate_year_partition_model.sql
@@ -3,7 +3,7 @@ This macro generates a year partition data model by receiving required
 attributes for the table. E.g., RAIN for RAIN_YYYY table.
 */
 
-{% macro generate_year_partition_model(attributes) %}
+{% macro generate_year_partition_model(attributes, year) %}
 
 select distinct
     station_name || '_' || to_varchar(date, 'yyyymmdd') as record_id,
@@ -13,8 +13,10 @@ select distinct
     state,
     current_date() as load_date
 from {{ source("staging", "weather_preprocessed") }} as source
+where extract(year from date) = {{ year }}
 {% if is_incremental() %}
-where not exists (
+where extract(year from date) = {{ year }}
+    and not exists (
     select 1
     from {{ this }}
     where (source.station_name || '_' || to_varchar(source.date, 'yyyymmdd')) = record_id
