fdf41b5c3799f31cb12507f862c84606089850ae
Travis Hong <travis.j.hong@gmail.com>
2023-11-12 23:39:38 +1100

    add files

--- airflow/dags/weather_analysis_pipeline.py
+++ airflow/dags/weather_analysis_pipeline.py
@@ -1,54 +1,38 @@
-
-"""Example DAG demonstrating the usage of the BashOperator."""
-
-from datetime import timedelta
-
+###############################################################################
+# Name: weather_analysis_pipeline.py
+# Description: This DAG script orchestrates data processes on Airflow. The flow
+#              is designed to send an alert email upon completion or failure.
+# Author: Travis Hong
+# Repository: https://github.com/TravisH0301/weather_analysis
+###############################################################################
+from datetime import datetime
 from airflow import DAG
-from airflow.operators.bash import BashOperator
-from airflow.operators.dummy import DummyOperator
-from airflow.utils.dates import days_ago
 
-args = {
-    'owner': 'airflow',
-}
+from utils.airflow_email import AirflowEmailSender
+from airflow.operators.bash import BashOperator
+
+
+# Instantiate Airflow email sender
+email_sender = AirflowEmailSender(
+    send_from="airflow-bot@test.com",
+    send_to="local-email@test.com"
+)
+
 
 with DAG(
-    dag_id='example_bash_operator',
-    default_args=args,
-    schedule_interval='0 0 * * *',
-    start_date=days_ago(2),
-    dagrun_timeout=timedelta(minutes=60),
-    tags=['example', 'example2'],
-    params={"example_key": "example_value"},
+    dag_id="Weather_Analysis",
+    default_args={
+        "owner": "Travis Hong",
+        "start_date": datetime(2023, 11, 12),
+        "schedule_interval": "@monthly",
+        "retries": 0,
+        "on_success_callback": email_sender.dag_complete_alert,
+        "on_failure_callback": email_sender.dag_failure_alert
+    }
 ) as dag:
-
-    run_this_last = DummyOperator(
-        task_id='run_this_last',
-    )
-
-    # [START howto_operator_bash]
+    
     run_this = BashOperator(
-        task_id='run_after_loop',
-        bash_command='echo 1',
+        task_id="first_job",
+        bash_command="echo 1",
+        dag=dag
     )
-    # [END howto_operator_bash]
-
-    run_this >> run_this_last
-
-    for i in range(3):
-        task = BashOperator(
-            task_id='runme_' + str(i),
-            bash_command='echo "{{ task_instance_key_str }}" && sleep 1',
-        )
-        task >> run_this
-
-    # [START howto_operator_bash_template]
-    also_run_this = BashOperator(
-        task_id='also_run_this',
-        bash_command='echo "run_id={{ run_id }} | dag_run={{ dag_run }}"',
-    )
-    # [END howto_operator_bash_template]
-    also_run_this >> run_this_last
-
-if __name__ == "__main__":
-    dag.cli()
