a8e171a5843d632e9144ce5838ed5cb7bdef2508
Travis Hong <travis.j.hong@gmail.com>
2023-11-13 09:34:07 +1100

    change local logger to airflow task logger & add try-except block at file load

--- airflow/dags/scripts/land_file.py
+++ airflow/dags/scripts/land_file.py
@@ -13,6 +13,8 @@ import pytz
 from urllib.request import urlopen
 
 import boto3
+from botocore.exceptions import ClientError
+from airflow.utils.log.logging_mixin import LoggingMixin
 
 
 def retrieve_ftp_file(ftp_file_path):
@@ -35,41 +37,34 @@ def retrieve_ftp_file(ftp_file_path):
 
 
 def main():
-    logging.info("Process has started")
+    LoggingMixin().log.info("Process has started")
 
     # Retrieve compressed file as byte stream object
-    logging.info("Retrieving compressed file...")
+    LoggingMixin().log.info("Retrieving compressed file...")
     comp_file = retrieve_ftp_file(ftp_file_path)
-    logging.info("Compressed file has been retrieved")
+    LoggingMixin().log.info("Compressed file has been retrieved")
 
     # Load compressed file into object storage
     """Current date is added to the file name to keep track of 
     BOM dataset version within the compressed file.
     """
-    logging.info("Loading compressed file into object storage...")
+    LoggingMixin().log.info("Loading compressed file into object storage...")
     file_name = os.path.basename(ftp_file_path)
     file_name_date = file_name[:-4] + f"_{date_today_str}" + file_name[-4:]
-    s3.put_object(
-        Bucket=bucket_name,
-        Key=file_name_date,
-        Body=comp_file
-    )
-    logging.info("Compressed file has been loaded to object storage")
+    try:
+        s3.put_object(
+            Bucket=bucket_name,
+            Key=file_name_date,
+            Body=comp_file
+        )
+    except ClientError as e:
+        LoggingMixin().log.error("File load has failed with an error: {e}")    
+    LoggingMixin().log.info("Compressed file has been loaded to object storage")
     
-
-    logging.info("Process has completed")
+    LoggingMixin().log.info("Process has completed")
 
 
 if __name__ == "__main__":
-    # Define variables
-    ## Logger
-    logging.basicConfig(
-        filename = "/opt/airflow/logs/land_file_log.txt",
-        filemode="w",
-        level=logging.INFO,
-        format = "%(asctime)s; %(levelname)s; %(message)s",
-        datefmt="%m/%d/%Y %I:%M:%S %p %Z"
-    )
     ## Date
     melb_tz = pytz.timezone('Australia/Melbourne')
     datetime_now = datetime.now(melb_tz)
@@ -88,9 +83,7 @@ if __name__ == "__main__":
         aws_secret_access_key=minio_secret_key
     )
 
-    try:
-        main()
-    except Exception:
-        logging.error("Process has failed:", exc_info=True)
-        raise
+    # Start Process
+    main()
+    
 
