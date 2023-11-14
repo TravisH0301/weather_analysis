ca84a794e8e91d43e2126a5495476afcc9c8d9b2
Travis Hong <travis.j.hong@gmail.com>
2023-11-14 20:38:21 +1100

    correct table referencing

--- airflow/dags/dbt/models/aggregated/monthly_average.sql
+++ airflow/dags/dbt/models/aggregated/monthly_average.sql
@@ -5,185 +5,185 @@
 }}
 
 with evapo_trans_union as (
-    select record_id, station_name, state, date, evapo_transpiration from evapo_transpiration.evapo_transpiration_2023 
+    select record_id, station_name, state, date, evapo_transpiration from {{ ref('evapo_transpiration_2023') }} 
     union
-    select record_id, station_name, state, date, evapo_transpiration from evapo_transpiration.evapo_transpiration_2022
+    select record_id, station_name, state, date, evapo_transpiration from {{ ref('evapo_transpiration_2022') }}
     union
-    select record_id, station_name, state, date, evapo_transpiration from evapo_transpiration.evapo_transpiration_2021
+    select record_id, station_name, state, date, evapo_transpiration from {{ ref('evapo_transpiration_2021') }}
     union
-    select record_id, station_name, state, date, evapo_transpiration from evapo_transpiration.evapo_transpiration_2020
+    select record_id, station_name, state, date, evapo_transpiration from {{ ref('evapo_transpiration_2020') }}
     union
-    select record_id, station_name, state, date, evapo_transpiration from evapo_transpiration.evapo_transpiration_2019
+    select record_id, station_name, state, date, evapo_transpiration from {{ ref('evapo_transpiration_2019') }}
     union
-    select record_id, station_name, state, date, evapo_transpiration from evapo_transpiration.evapo_transpiration_2018
+    select record_id, station_name, state, date, evapo_transpiration from {{ ref('evapo_transpiration_2018') }}
     union
-    select record_id, station_name, state, date, evapo_transpiration from evapo_transpiration.evapo_transpiration_2017
+    select record_id, station_name, state, date, evapo_transpiration from {{ ref('evapo_transpiration_2017') }}
     union
-    select record_id, station_name, state, date, evapo_transpiration from evapo_transpiration.evapo_transpiration_2016
+    select record_id, station_name, state, date, evapo_transpiration from {{ ref('evapo_transpiration_2016') }}
     union
-    select record_id, station_name, state, date, evapo_transpiration from evapo_transpiration.evapo_transpiration_2015
+    select record_id, station_name, state, date, evapo_transpiration from {{ ref('evapo_transpiration_2015') }}
     union
-    select record_id, station_name, state, date, evapo_transpiration from evapo_transpiration.evapo_transpiration_2014
+    select record_id, station_name, state, date, evapo_transpiration from {{ ref('evapo_transpiration_2014') }}
     union
-    select record_id, station_name, state, date, evapo_transpiration from evapo_transpiration.evapo_transpiration_2013
+    select record_id, station_name, state, date, evapo_transpiration from {{ ref('evapo_transpiration_2013') }}
     union
-    select record_id, station_name, state, date, evapo_transpiration from evapo_transpiration.evapo_transpiration_2012
+    select record_id, station_name, state, date, evapo_transpiration from {{ ref('evapo_transpiration_2012') }}
 ),
 
 rain_union as (
-    select record_id, station_name, state, date, rain from rain.rain_2023 
+    select record_id, station_name, state, date, rain from {{ ref('rain_2023') }} 
     union
-    select record_id, station_name, state, date, rain from rain.rain_2022
+    select record_id, station_name, state, date, rain from {{ ref('rain_2022') }}
     union
-    select record_id, station_name, state, date, rain from rain.rain_2021
+    select record_id, station_name, state, date, rain from {{ ref('rain_2021') }}
     union
-    select record_id, station_name, state, date, rain from rain.rain_2020
+    select record_id, station_name, state, date, rain from {{ ref('rain_2020') }}
     union
-    select record_id, station_name, state, date, rain from rain.rain_2019
+    select record_id, station_name, state, date, rain from {{ ref('rain_2019') }}
     union
-    select record_id, station_name, state, date, rain from rain.rain_2018
+    select record_id, station_name, state, date, rain from {{ ref('rain_2018') }}
     union
-    select record_id, station_name, state, date, rain from rain.rain_2017
+    select record_id, station_name, state, date, rain from {{ ref('rain_2017') }}
     union
-    select record_id, station_name, state, date, rain from rain.rain_2016
+    select record_id, station_name, state, date, rain from {{ ref('rain_2016') }}
     union
-    select record_id, station_name, state, date, rain from rain.rain_2015
+    select record_id, station_name, state, date, rain from {{ ref('rain_2015') }}
     union
-    select record_id, station_name, state, date, rain from rain.rain_2014
+    select record_id, station_name, state, date, rain from {{ ref('rain_2014') }}
     union
-    select record_id, station_name, state, date, rain from rain.rain_2013
+    select record_id, station_name, state, date, rain from {{ ref('rain_2013') }}
     union
-    select record_id, station_name, state, date, rain from rain.rain_2012
+    select record_id, station_name, state, date, rain from {{ ref('rain_2012') }}
 ),
 
 pan_evapo_union as (
-    select record_id, station_name, state, date, pan_evaporation from pan_evaporation.pan_evaporation_2023 
+    select record_id, station_name, state, date, pan_evaporation from {{ ref('pan_evaporation_2023') }} 
     union
-    select record_id, station_name, state, date, pan_evaporation from pan_evaporation.pan_evaporation_2022
+    select record_id, station_name, state, date, pan_evaporation from {{ ref('pan_evaporation_2022') }}
     union
-    select record_id, station_name, state, date, pan_evaporation from pan_evaporation.pan_evaporation_2021
+    select record_id, station_name, state, date, pan_evaporation from {{ ref('pan_evaporation_2021') }}
     union
-    select record_id, station_name, state, date, pan_evaporation from pan_evaporation.pan_evaporation_2020
+    select record_id, station_name, state, date, pan_evaporation from {{ ref('pan_evaporation_2020') }}
     union
-    select record_id, station_name, state, date, pan_evaporation from pan_evaporation.pan_evaporation_2019
+    select record_id, station_name, state, date, pan_evaporation from {{ ref('pan_evaporation_2019') }}
     union
-    select record_id, station_name, state, date, pan_evaporation from pan_evaporation.pan_evaporation_2018
+    select record_id, station_name, state, date, pan_evaporation from {{ ref('pan_evaporation_2018') }}
     union
-    select record_id, station_name, state, date, pan_evaporation from pan_evaporation.pan_evaporation_2017
+    select record_id, station_name, state, date, pan_evaporation from {{ ref('pan_evaporation_2017') }}
     union
-    select record_id, station_name, state, date, pan_evaporation from pan_evaporation.pan_evaporation_2016
+    select record_id, station_name, state, date, pan_evaporation from {{ ref('pan_evaporation_2016') }}
     union
-    select record_id, station_name, state, date, pan_evaporation from pan_evaporation.pan_evaporation_2015
+    select record_id, station_name, state, date, pan_evaporation from {{ ref('pan_evaporation_2015') }}
     union
-    select record_id, station_name, state, date, pan_evaporation from pan_evaporation.pan_evaporation_2014
+    select record_id, station_name, state, date, pan_evaporation from {{ ref('pan_evaporation_2014') }}
     union
-    select record_id, station_name, state, date, pan_evaporation from pan_evaporation.pan_evaporation_2013
+    select record_id, station_name, state, date, pan_evaporation from {{ ref('pan_evaporation_2013') }}
     union
-    select record_id, station_name, state, date, pan_evaporation from pan_evaporation.pan_evaporation_2012
+    select record_id, station_name, state, date, pan_evaporation from {{ ref('pan_evaporation_2012') }}
 ),
 
 temp_union as (
-    select record_id, station_name, state, date, variance_temperature from temperature.temperature_2023 
+    select record_id, station_name, state, date, variance_temperature from {{ ref('temperature_2023') }} 
     union
-    select record_id, station_name, state, date, variance_temperature from temperature.temperature_2022
+    select record_id, station_name, state, date, variance_temperature from {{ ref('temperature_2022') }}
     union
-    select record_id, station_name, state, date, variance_temperature from temperature.temperature_2021
+    select record_id, station_name, state, date, variance_temperature from {{ ref('temperature_2021') }}
     union
-    select record_id, station_name, state, date, variance_temperature from temperature.temperature_2020
+    select record_id, station_name, state, date, variance_temperature from {{ ref('temperature_2020') }}
     union
-    select record_id, station_name, state, date, variance_temperature from temperature.temperature_2019
+    select record_id, station_name, state, date, variance_temperature from {{ ref('temperature_2019') }}
     union
-    select record_id, station_name, state, date, variance_temperature from temperature.temperature_2018
+    select record_id, station_name, state, date, variance_temperature from {{ ref('temperature_2018') }}
     union
-    select record_id, station_name, state, date, variance_temperature from temperature.temperature_2017
+    select record_id, station_name, state, date, variance_temperature from {{ ref('temperature_2017') }}
     union
-    select record_id, station_name, state, date, variance_temperature from temperature.temperature_2016
+    select record_id, station_name, state, date, variance_temperature from {{ ref('temperature_2016') }}
     union
-    select record_id, station_name, state, date, variance_temperature from temperature.temperature_2015
+    select record_id, station_name, state, date, variance_temperature from {{ ref('temperature_2015') }}
     union
-    select record_id, station_name, state, date, variance_temperature from temperature.temperature_2014
+    select record_id, station_name, state, date, variance_temperature from {{ ref('temperature_2014') }}
     union
-    select record_id, station_name, state, date, variance_temperature from temperature.temperature_2013
+    select record_id, station_name, state, date, variance_temperature from {{ ref('temperature_2013') }}
     union
-    select record_id, station_name, state, date, variance_temperature from temperature.temperature_2012
+    select record_id, station_name, state, date, variance_temperature from {{ ref('temperature_2012') }}
 ),
 
 rel_hum_union as (
-    select record_id, station_name, state, date, maximum_relative_humidity, minimum_relative_humidity from relative_humidity.relative_humidity_2023 
+    select record_id, station_name, state, date, maximum_relative_humidity, minimum_relative_humidity from {{ ref('relative_humidity_2023') }} 
     union
-    select record_id, station_name, state, date, maximum_relative_humidity, minimum_relative_humidity from relative_humidity.relative_humidity_2022
+    select record_id, station_name, state, date, maximum_relative_humidity, minimum_relative_humidity from {{ ref('relative_humidity_2022') }}
     union
-    select record_id, station_name, state, date, maximum_relative_humidity, minimum_relative_humidity from relative_humidity.relative_humidity_2021
+    select record_id, station_name, state, date, maximum_relative_humidity, minimum_relative_humidity from {{ ref('relative_humidity_2021') }}
     union
-    select record_id, station_name, state, date, maximum_relative_humidity, minimum_relative_humidity from relative_humidity.relative_humidity_2020
+    select record_id, station_name, state, date, maximum_relative_humidity, minimum_relative_humidity from {{ ref('relative_humidity_2020') }}
     union
-    select record_id, station_name, state, date, maximum_relative_humidity, minimum_relative_humidity from relative_humidity.relative_humidity_2019
+    select record_id, station_name, state, date, maximum_relative_humidity, minimum_relative_humidity from {{ ref('relative_humidity_2019') }}
     union
-    select record_id, station_name, state, date, maximum_relative_humidity, minimum_relative_humidity from relative_humidity.relative_humidity_2018
+    select record_id, station_name, state, date, maximum_relative_humidity, minimum_relative_humidity from {{ ref('relative_humidity_2018') }}
     union
-    select record_id, station_name, state, date, maximum_relative_humidity, minimum_relative_humidity from relative_humidity.relative_humidity_2017
+    select record_id, station_name, state, date, maximum_relative_humidity, minimum_relative_humidity from {{ ref('relative_humidity_2017') }}
     union
-    select record_id, station_name, state, date, maximum_relative_humidity, minimum_relative_humidity from relative_humidity.relative_humidity_2016
+    select record_id, station_name, state, date, maximum_relative_humidity, minimum_relative_humidity from {{ ref('relative_humidity_2016') }}
     union
-    select record_id, station_name, state, date, maximum_relative_humidity, minimum_relative_humidity from relative_humidity.relative_humidity_2015
+    select record_id, station_name, state, date, maximum_relative_humidity, minimum_relative_humidity from {{ ref('relative_humidity_2015') }}
     union
-    select record_id, station_name, state, date, maximum_relative_humidity, minimum_relative_humidity from relative_humidity.relative_humidity_2014
+    select record_id, station_name, state, date, maximum_relative_humidity, minimum_relative_humidity from {{ ref('relative_humidity_2014') }}
     union
-    select record_id, station_name, state, date, maximum_relative_humidity, minimum_relative_humidity from relative_humidity.relative_humidity_2013
+    select record_id, station_name, state, date, maximum_relative_humidity, minimum_relative_humidity from {{ ref('relative_humidity_2013') }}
     union
-    select record_id, station_name, state, date, maximum_relative_humidity, minimum_relative_humidity from relative_humidity.relative_humidity_2012
+    select record_id, station_name, state, date, maximum_relative_humidity, minimum_relative_humidity from {{ ref('relative_humidity_2012') }}
 ),
 
 wind_speed_union as (
-    select record_id, station_name, state, date, average_10m_wind_speed from wind_speed.wind_speed_2023 
+    select record_id, station_name, state, date, average_10m_wind_speed from {{ ref('wind_speed_2023') }} 
     union
-    select record_id, station_name, state, date, average_10m_wind_speed from wind_speed.wind_speed_2022
+    select record_id, station_name, state, date, average_10m_wind_speed from {{ ref('wind_speed_2022') }}
     union
-    select record_id, station_name, state, date, average_10m_wind_speed from wind_speed.wind_speed_2021
+    select record_id, station_name, state, date, average_10m_wind_speed from {{ ref('wind_speed_2021') }}
     union
-    select record_id, station_name, state, date, average_10m_wind_speed from wind_speed.wind_speed_2020
+    select record_id, station_name, state, date, average_10m_wind_speed from {{ ref('wind_speed_2020') }}
     union
-    select record_id, station_name, state, date, average_10m_wind_speed from wind_speed.wind_speed_2019
+    select record_id, station_name, state, date, average_10m_wind_speed from {{ ref('wind_speed_2019') }}
     union
-    select record_id, station_name, state, date, average_10m_wind_speed from wind_speed.wind_speed_2018
+    select record_id, station_name, state, date, average_10m_wind_speed from {{ ref('wind_speed_2018') }}
     union
-    select record_id, station_name, state, date, average_10m_wind_speed from wind_speed.wind_speed_2017
+    select record_id, station_name, state, date, average_10m_wind_speed from {{ ref('wind_speed_2017') }}
     union
-    select record_id, station_name, state, date, average_10m_wind_speed from wind_speed.wind_speed_2016
+    select record_id, station_name, state, date, average_10m_wind_speed from {{ ref('wind_speed_2016') }}
     union
-    select record_id, station_name, state, date, average_10m_wind_speed from wind_speed.wind_speed_2015
+    select record_id, station_name, state, date, average_10m_wind_speed from {{ ref('wind_speed_2015') }}
     union
-    select record_id, station_name, state, date, average_10m_wind_speed from wind_speed.wind_speed_2014
+    select record_id, station_name, state, date, average_10m_wind_speed from {{ ref('wind_speed_2014') }}
     union
-    select record_id, station_name, state, date, average_10m_wind_speed from wind_speed.wind_speed_2013
+    select record_id, station_name, state, date, average_10m_wind_speed from {{ ref('wind_speed_2013') }}
     union
-    select record_id, station_name, state, date, average_10m_wind_speed from wind_speed.wind_speed_2012
+    select record_id, station_name, state, date, average_10m_wind_speed from {{ ref('wind_speed_2012') }}
 ),
 
 solar_rad_union as (
-    select record_id, station_name, state, date, solar_radiation from solar_radiation.solar_radiation_2023 
+    select record_id, station_name, state, date, solar_radiation from {{ ref('solar_radiation_2023') }} 
     union
-    select record_id, station_name, state, date, solar_radiation from solar_radiation.solar_radiation_2022
+    select record_id, station_name, state, date, solar_radiation from {{ ref('solar_radiation_2022') }}
     union
-    select record_id, station_name, state, date, solar_radiation from solar_radiation.solar_radiation_2021
+    select record_id, station_name, state, date, solar_radiation from {{ ref('solar_radiation_2021') }}
     union
-    select record_id, station_name, state, date, solar_radiation from solar_radiation.solar_radiation_2020
+    select record_id, station_name, state, date, solar_radiation from {{ ref('solar_radiation_2020') }}
     union
-    select record_id, station_name, state, date, solar_radiation from solar_radiation.solar_radiation_2019
+    select record_id, station_name, state, date, solar_radiation from {{ ref('solar_radiation_2019') }}
     union
-    select record_id, station_name, state, date, solar_radiation from solar_radiation.solar_radiation_2018
+    select record_id, station_name, state, date, solar_radiation from {{ ref('solar_radiation_2018') }}
     union
-    select record_id, station_name, state, date, solar_radiation from solar_radiation.solar_radiation_2017
+    select record_id, station_name, state, date, solar_radiation from {{ ref('solar_radiation_2017') }}
     union
-    select record_id, station_name, state, date, solar_radiation from solar_radiation.solar_radiation_2016
+    select record_id, station_name, state, date, solar_radiation from {{ ref('solar_radiation_2016') }}
     union
-    select record_id, station_name, state, date, solar_radiation from solar_radiation.solar_radiation_2015
+    select record_id, station_name, state, date, solar_radiation from {{ ref('solar_radiation_2015') }}
     union
-    select record_id, station_name, state, date, solar_radiation from solar_radiation.solar_radiation_2014
+    select record_id, station_name, state, date, solar_radiation from {{ ref('solar_radiation_2014') }}
     union
-    select record_id, station_name, state, date, solar_radiation from solar_radiation.solar_radiation_2013
+    select record_id, station_name, state, date, solar_radiation from {{ ref('solar_radiation_2013') }}
     union
-    select record_id, station_name, state, date, solar_radiation from solar_radiation.solar_radiation_2012
+    select record_id, station_name, state, date, solar_radiation from {{ ref('solar_radiation_2012') }}
 ),
 
 join_group as (
